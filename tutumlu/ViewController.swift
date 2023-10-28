d768fb4f43a53075516352b44cca2083638571e4
ali-kerem <alikerembozkurtt@gmail.com>
2023-10-28 18:05:55 +0300

    Initial commit

---
+++ tutumlu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tutumlu
+//
+//  Created by ali-kerem on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
