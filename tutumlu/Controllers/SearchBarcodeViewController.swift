3379dfe3e7e586d798c4bc8595dc5d763e9eeaa4
ali-kerem <alikerembozkurtt@gmail.com>
2023-12-31 18:17:45 +0300

    Fix camera permission error

--- tutumlu/Controllers/SearchBarcodeViewController.swift
+++ tutumlu/Controllers/SearchBarcodeViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class SearchBarcodeViewController: UIViewController {
 
@@ -34,12 +36,53 @@ class SearchBarcodeViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    
     @objc private func scanButtonTapped() {
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+
+        switch cameraAuthorizationStatus {
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                DispatchQueue.main.async {
+                    if granted {
+                        self.navigateToScanVC()
+                    } else {
+                        self.showCameraAccessDeniedAlert()
+                    }
+                }
+            }
+        case .authorized: // The user has previously granted access to the camera.
+            self.navigateToScanVC()
+        case .restricted, .denied: // The user can't grant access due to restrictions or has previously denied access.
+            self.showCameraAccessDeniedAlert()
+        @unknown default:
+            fatalError("Unknown camera authorization status")
+        }
+    }
+
+    private func navigateToScanVC() {
         let scanVC = BarcodeScanViewController()
         scanVC.delegate = self
         navigationController?.pushViewController(scanVC, animated: true)
-        
     }
+
+    private func showCameraAccessDeniedAlert() {
+        let alert = UIAlertController(
+            title: "Camera Access Denied",
+            message: "Please enable camera access for this app in your device settings to use the barcode scanning feature.",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        alert.addAction(UIAlertAction(title: "Settings", style: .cancel) { _ in
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl)
+            }
+        })
+        present(alert, animated: true)
+    }
+
+    
 }
 
 
